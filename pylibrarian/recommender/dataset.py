8ef21f02d72081704b4d164ade451257ece900cc
Simon Popelier <simon.popelier@gmail.com>
2023-07-02 19:11:26 +0200

    :card_file_box: Dataset class for training recommender.

---
+++ pylibrarian/recommender/dataset.py
@@ -0,0 +1,13 @@
+from google.cloud import bigquery
+import pandas as pd
+
+class Dataset():
+    def __init__(self, project: str = None):
+        self.client = bigquery.Client()
+        self.project = project
+
+    def load_data(self) -> pd.DataFrame:
+        QUERY = "SELECT package, requirement FROM the-sandbox-386618.pylibrarian.requirements"
+        query_job = self.client.query(QUERY, project=self.project)
+        rows = query_job.result()
+        return pd.DataFrame([{"package": r.package, "requirement":  r.requirement} for r in rows])

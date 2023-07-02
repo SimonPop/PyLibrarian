660c10878eec695c87f4a659b9e6f8c6f44812e9
Simon Popelier <simon.popelier@gmail.com>
2023-07-02 19:16:24 +0200

    :art: Formatting with Black.

--- pylibrarian/recommender/dataset.py
+++ pylibrarian/recommender/dataset.py
@@ -1,7 +1,8 @@
 from google.cloud import bigquery
 import pandas as pd
 
-class Dataset():
+
+class Dataset:
     def __init__(self, project: str = None):
         self.client = bigquery.Client()
         self.project = project
@@ -10,4 +11,6 @@ class Dataset():
         QUERY = "SELECT package, requirement FROM the-sandbox-386618.pylibrarian.requirements"
         query_job = self.client.query(QUERY, project=self.project)
         rows = query_job.result()
-        return pd.DataFrame([{"package": r.package, "requirement":  r.requirement} for r in rows])
+        return pd.DataFrame(
+            [{"package": r.package, "requirement": r.requirement} for r in rows]
+        )
--- pylibrarian/scrapper/pypi_scapper.py
+++ pylibrarian/scrapper/pypi_scapper.py
@@ -7,18 +7,26 @@ from tqdm import tqdm
 from pathlib import Path
 from google.cloud import bigquery
 
-class PypiScrapper():
-    def __init__(self, output_file: str = None, package_source: str = "packages.txt", project: str = None):
+
+class PypiScrapper:
+    def __init__(
+        self,
+        output_file: str = None,
+        package_source: str = "packages.txt",
+        project: str = None,
+    ):
 
         # Package source:
-        self.package_source = Path(__file__).parent.parent.parent / "data" / package_source
+        self.package_source = (
+            Path(__file__).parent.parent.parent / "data" / package_source
+        )
 
         # Local output:
         self.output_file = output_file
         if output_file is not None:
             raise NotImplementedError("Local output file is not implemented.")
-        
-        # Database: 
+
+        # Database:
         self.client = bigquery.Client()
         self.project = project
 
@@ -42,15 +50,15 @@ class PypiScrapper():
     def package_done_local(self) -> List[str]:
 
         with open(self.output_file, "r") as f:
-            done_packages = [l.split(';')[0] for l in f.readlines()]
-        
+            done_packages = [l.split(";")[0] for l in f.readlines()]
+
         return done_packages
-    
+
     def package_done_table(self) -> List[str]:
         QUERY = f"SELECT DISTINCT package FROM {self.project}.pylibrarian.requirements"
         query_job = self.client.query(QUERY, project=self.project)
         rows = query_job.result()
-        return  [r.package for r in rows]
+        return [r.package for r in rows]
 
     def get_requirements(self, package_name: str) -> List[str]:
         request = requests.get(f"https://pypi.org/pypi/{package_name}/json")
@@ -59,10 +67,10 @@ class PypiScrapper():
             return []
         else:
             content = request.content
-            requires_dist = json.loads(content)['info']['requires_dist']
+            requires_dist = json.loads(content)["info"]["requires_dist"]
             if requires_dist is None:
                 requires_dist = []
-            return [r.split(' ')[0] for r in requires_dist]
+            return [r.split(" ")[0] for r in requires_dist]
 
     def scrap(self):
         pbar = tqdm(self.packages)
@@ -80,12 +88,12 @@ class PypiScrapper():
         ]
 
         if len(rows) > 0:
-            errors = self.client.insert_rows_json(f"{self.project}.pylibrarian.requirements", rows)
+            errors = self.client.insert_rows_json(
+                f"{self.project}.pylibrarian.requirements", rows
+            )
 
         if self.output_file is not None:
             row = f"{package};{requirements}"
             with open(self.output_file, "a") as f:
-                f.write(
-                    row
-                )
+                f.write(row)
                 f.write("\n")

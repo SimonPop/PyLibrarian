780d13cdc2c4de20727c1ae54835fab27e1fdb29
Simon Popelier <simon.popelier@gmail.com>
2023-07-02 19:31:28 +0200

    :card_file_box: Filtering in BigQuery requirement query.

--- pylibrarian/recommender/dataset.py
+++ pylibrarian/recommender/dataset.py
@@ -6,9 +6,15 @@ class Dataset:
     def __init__(self, project: str = None):
         self.client = bigquery.Client()
         self.project = project
+        self.threshold = 5 # Minimum number of mentions of a package.
 
     def load_data(self) -> pd.DataFrame:
-        QUERY = "SELECT package, requirement FROM the-sandbox-386618.pylibrarian.requirements"
+        FILTER_QUERY = f"SELECT requirement, COUNT(*) FROM `{self.project}.pylibrarian.requirements` GROUP BY requirement HAVING COUNT(*) > 1"
+
+        QUERY = (
+            f"SELECT package, requirement FROM {self.project}.pylibrarian.requirements"
+            f"WHERE requirement IN {FILTER_QUERY}"
+            )
         query_job = self.client.query(QUERY, project=self.project)
         rows = query_job.result()
         return pd.DataFrame(
